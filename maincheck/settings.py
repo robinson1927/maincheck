cce3fcb0398a3cceafbf319e0708b46e7dec1b85
robinson1927 <robinsond.gananb@uqvirtual.edu.co>
2025-05-12 01:14:58 -0500

    ultimos cambios

--- maincheck/settings.py
+++ maincheck/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v&#z83@$!ex)_l%3lrzn*d=mc5m40xt3n14yqh*8p1k2b^5ah6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
