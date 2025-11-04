f9b3eb9b510b53260dacb1d1a35a0c3249467f24
robinson1927 <robinsond.gananb@uqvirtual.edu.co>
2025-11-03 21:29:03 -0500

    configuracion de host

--- maincheck/settings.py
+++ maincheck/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 if 'RENDER' in os.environ:
     ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
